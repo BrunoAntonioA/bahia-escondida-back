6cec20be7b427bcfe1d61f97dfe5250cf8fd2011
baguilar-btf <baguilar@betterfly.com>
2026-01-08 16:33:09 -0300

    fix: cors for front end

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://bahia-escondida-front.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
